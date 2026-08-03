03b2bf63dcfd89bad221a4a4be1ba382be5f1932
lelh2305-creu <lelh2305@gmail.com>
2026-08-03 19:01:18 +0700

    style: apply Option 2 font pairing with Be Vietnam Pro (Regular) and Playfair Display (Italic)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,300..700;1,9..40,300..700&family=Manrope:wght@300;400;500;600;700;800&family=Playfair+Display:ital,wght@0,400..800;1,400..800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&family=Playfair+Display:ital,wght@0,400..800;1,400..800&display=swap"
           rel="stylesheet"
         />
       </head>
